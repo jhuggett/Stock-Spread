ff88efebf7707872f993cd3c8655791377ac9580
Joel Huggett <joel@forestry.io>
2020-05-05 21:17:14 -0300

    Initial Commit

---
+++ Stock Spread/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Spread
+//
+//  Created by Joel Huggett on 2020-05-05.
+//  Copyright © 2020 Joel Huggett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
