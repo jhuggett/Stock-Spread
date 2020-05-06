9abb9030f74bc607f630d085d382fbd567d15af3
Joel Huggett <joel@forestry.io>
2020-05-06 08:57:44 -0300

    initial commit

---
+++ Stock Spread/StockHandler.swift
@@ -0,0 +1,21 @@
+//
+//  StockHandler.swift
+//  Stock Spread
+//
+//  Created by Joel Huggett on 2020-05-05.
+//  Copyright © 2020 Joel Huggett. All rights reserved.
+//
+
+import Foundation
+
+class StockHandler {
+    
+    
+    static func getPersistantHandler() -> StockHandler {
+        let defaults = UserDefaults.standard
+        
+        if defaults.bool(forKey: "")
+    }
+    
+    
+}
